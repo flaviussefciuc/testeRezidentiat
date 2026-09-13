82959467744c15e88cb75e5b1e389781af36e2d8
flaviussefciuc <flaviussefciuc@users.noreply.github.com>
2026-09-13 17:22:21 +0300

    manually update questions.ts

--- src/app/questions.ts
+++ src/app/questions.ts
@@ -10,10 +10,13 @@ import {ADVANCED_REVISIONS} from './bank/advanced-revisions';
 import {RHEUMATOLOGY_ADVANCED} from './bank/rheumatology-advanced';
 import {THYROID_ADVANCED} from './bank/thyroid-advanced';
 import {DIABETES_ADVANCED} from './bank/diabetes-advanced';
+import {DIABETES_COMPLICATIONS_ADVANCED} from './bank/diabetes-complications-advanced';
+import {TROMBOEMBOLISM_ADVANCED} from './bank/tromboembolism-advanced';
+import {HIPERTENSIUNE_ADVANCED} from './bank/hipertensiune-advanced';
 import {advancedEligibilityErrors} from './bank/advanced-author';
 
 export const PREVIOUS_SOURCE_QUESTIONS:Question[]=[...SEPSIS_SOURCE,...ELECTROLYTES_SOURCE,...INTENSIVE_CARE_SOURCE,...HEMATOLOGY_SOURCE];
-export const ADVANCED_CANDIDATES=[...ADVANCED_REVISIONS,...RHEUMATOLOGY_ADVANCED,...THYROID_ADVANCED,...DIABETES_ADVANCED];
+export const ADVANCED_CANDIDATES=[...ADVANCED_REVISIONS,...RHEUMATOLOGY_ADVANCED,...THYROID_ADVANCED,...DIABETES_ADVANCED,...DIABETES_COMPLICATIONS_ADVANCED,...TROMBOEMBOLISM_ADVANCED,...HIPERTENSIUNE_ADVANCED];
 export const SOURCE_CHECKED_QUESTIONS=ADVANCED_CANDIDATES.filter(q=>advancedEligibilityErrors(q).length===0);
 export const LEGACY_QUESTIONS:Question[]=[...KUMAR,...SURGERY,...SYNOPSIS];
 // Old stems/options/keys remain addressable for saved attempts and open drafts.
